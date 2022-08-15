7386e47e79063128f65ea0b5ce20007bf1f41ef0
EunjinJwa <ejjwa0563@gmail.com>
2022-08-15 18:29:45 +0900

    date

--- hello_python/date.py
+++ hello_python/date.py
@@ -1,10 +1,8 @@
 import datetime
 
-
 now = datetime.datetime.now()
 
 date_str = f"{now.year}년 {now.month}월 {now.day}일 {now.hour}시 {now.minute}분 {now.second}초"
 print(date_str)
-print(now.date)
 
 
