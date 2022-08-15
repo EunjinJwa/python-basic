41cd14c08647b62f8943f8afa1f69cbcf138a01e
EunjinJwa <ejjwa0563@gmail.com>
2022-08-15 14:31:19 +0900

    dictionary

---
+++ hello_python/dictionary_ex01.py
@@ -0,0 +1,20 @@
+# dictionary : {key, value}
+
+dictionary = {
+    "name": "건조 딸기",
+    "type": "당절임",
+    "origin": "필리핀",
+    "ingredient": ["딸기", "설탕", "색소"]
+}
+
+print("dictionary 요소 출력 : ", dictionary["name"])
+
+dictionary["name"] = "건조 딸기 pro"
+print("dictionary 요소 출력 : ", dictionary["name"])
+
+if dictionary.get("price") == None:
+    dictionary["price"] = 5000
+print(dictionary)
+
+for key in dictionary:
+    print(key, " : ", dictionary[key])
---
+++ hello_python/for_ex01.py
@@ -0,0 +1,15 @@
+list_of_list = [
+    [1,2,3],
+    [4,5,6,7],
+    [8,9]
+]
+
+for element in list_of_list:
+    for item in element:
+        print("item : ", item)
+    print("element : ", element)
+
+# 전개연산자 * : 리스트 요소를 전개하여 입력한 것과 같은 효과 
+# *리스트 -> 리스트[0], 리스트[1],... 
+print(list_of_list[0])      # [1, 2, 3]
+print(*list_of_list[0])     # 1 2 3
